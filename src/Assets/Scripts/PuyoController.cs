78282a555d458ec4596fd126f7c15555b6083618
TakumuTakanezawa <takanen20010829kota@gmail.com>
2025-01-17 05:45:55 +0900

    step2

--- src/Assets/Scripts/PuyoController.cs
+++ src/Assets/Scripts/PuyoController.cs
@@ -2,17 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PuyoType
+{
+    Blank = 0,
+
+    Green = 1,
+    Red = 2,
+    Yellow = 3,
+    Blue = 4,
+    Purple = 5,
+    Cyan = 6,
+
+    Invalid = 7,
+};
+
+[RequireComponent(typeof(Renderer))]
 public class PuyoController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    static readonly Color[] color_table = new Color[] { // PuyoController の中でしか色の値を制御できないようにカプセル化
+        Color.black,
+
+        Color.green,
+        Color.red,
+        Color.yellow,
+        Color.blue,
+        Color.magenta,
+        Color.cyan,
+
+        Color.gray,
+    };
+
+    [SerializeField] Renderer my_renderer = default!;// 自分自身のマテリアルを登録しておく(GetComponentをなくす)
+    PuyoType _type = PuyoType.Invalid;
+    // // Start is called before the first frame update
+    // void Start()
+    // {
         
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
+    public void SetPuyoType(PuyoType type)
+    {
+        _type = type;
+
+        my_renderer.material.color = color_table[(int)_type];
+    }
+    public PuyoType GetPuyoType()
+    {
+        return _type;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetPos(Vector3 pos)
     {
-        
+        this.transform.localPosition = pos;
     }
 }
