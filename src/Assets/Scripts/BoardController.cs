d9adb2f684ad9f11fa6fe712ac3c1ff1f982ad9d
TakumuTakanezawa <takanen20010829kota@gmail.com>
2025-01-17 07:36:43 +0900

    step3

---
+++ src/Assets/Scripts/BoardController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardController : MonoBehaviour
+{
+    public const int BOARD_WIDTH = 6;
+    public const int BOARD_HEIGHT = 14;
+
+    [SerializeField] GameObject prefabPuyo = default!;
+
+    int[,] _board = new int[BOARD_HEIGHT, BOARD_WIDTH];
+    GameObject[,] _Puyos = new GameObject[BOARD_HEIGHT, BOARD_WIDTH];
+
+    private void ClearAll()
+    {
+        for (int y = 0; y < BOARD_HEIGHT; y++)
+        {
+            for (int x = 0; x < BOARD_WIDTH; x++)
+            {
+                _board[y, x] = 0;
+
+                if (_Puyos[y, x] != null) Destroy(_Puyos[y, x]);
+                _Puyos[y, x] = null;
+            }
+        }
+    }
+
+    public void Start()
+    {
+        ClearAll();
+
+        for (int y = 0; y < BOARD_HEIGHT; y++)
+        {
+            for (int x = 0; x < BOARD_WIDTH; x++)
+            {
+                Settle(new Vector2Int(x, y), Random.Range(1, 7));
+            }
+        }
+    }
+
+    public static bool IsValidated(Vector2Int pos)
+    {
+        return 0 <= pos.x && pos.x < BOARD_WIDTH
+            && 0 <= pos.y && pos.y < BOARD_HEIGHT;
+    }
+
+    public bool CanSettle(Vector2Int pos)
+    {
+        if (!IsValidated(pos)) return false;
+
+        return 0 == _board[pos.y, pos.x];
+    }
+
+    public bool Settle(Vector2Int pos, int val)
+    {
+        if (!CanSettle(pos)) return false;
+
+        _board[pos.y, pos.x] = val;
+
+        Debug.Assert(_Puyos[pos.y, pos.x] == null);
+        Vector3 world_position = transform.position + new Vector3(pos.x, pos.y, 0.0f);
+        _Puyos[pos.y, pos.x] = Instantiate(prefabPuyo, world_position, Quaternion.identity, transform);
+        _Puyos[pos.y, pos.x].GetComponent<PuyoController>().SetPuyoType((PuyoType)val);
+
+        return true;
+    }
+}
